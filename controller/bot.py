acd13283d197ce1c55a50d714e5d90264d6027d0
yuuki08noah <yuuki08noah@gmail.com>
2024-12-26 08:08:17 +0900

    FEAT :: SOLVED API TEST

--- controller/bot.py
+++ controller/bot.py
@@ -1,5 +1,8 @@
 from discord.ext import commands
 from discord import Intents
+import requests
+from bs4 import BeautifulSoup
+import re
 
 bot = commands.Bot(command_prefix='$', intents=Intents.all())
 prefix = '$'
@@ -16,6 +19,13 @@ async def on_message(message):
 
     if message.content.startswith("$hello"):
         await message.channel.send("HELLO")
+
+    if message.content.startswith("$user"):
+        args = message.content.split()
+        response = requests.get(f"https://solved.ac/api/v3/user/grass?handle={args[1]}&topic=default")
+        print(response.json())
+        userInfo = requests.get("https://solved.ac/api/v3/user/show?handle=" + args[1])
+        await message.channel.send(response.json()["currentStreak"])
 
 async def start(token):
     await bot.start(token)
